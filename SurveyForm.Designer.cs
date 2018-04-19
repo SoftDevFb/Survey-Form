b8912c73975d51c95f859fd77648791638c1abd3
SoftDev <softwaredevfb@gmail.com>
2018-04-19 17:02:33 -0500

    Initial Commit

---
+++ SurveyForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Proj7SurveyForm
+{
+    partial class SurveyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radCSharp = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radWebDev = new System.Windows.Forms.RadioButton();
+            this.radSystems = new System.Windows.Forms.RadioButton();
+            this.chkBxAdvisor = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtBxName = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Full Name:";
+            // 
+            // radCSharp
+            // 
+            this.radCSharp.AutoSize = true;
+            this.radCSharp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radCSharp.Location = new System.Drawing.Point(17, 172);
+            this.radCSharp.Name = "radCSharp";
+            this.radCSharp.Size = new System.Drawing.Size(50, 22);
+            this.radCSharp.TabIndex = 4;
+            this.radCSharp.Text = "C#";
+            this.radCSharp.UseVisualStyleBackColor = true;
+            this.radCSharp.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Classes Chosen:";
+            // 
+            // radWebDev
+            // 
+            this.radWebDev.AutoSize = true;
+            this.radWebDev.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radWebDev.Location = new System.Drawing.Point(99, 172);
+            this.radWebDev.Name = "radWebDev";
+            this.radWebDev.Size = new System.Drawing.Size(97, 22);
+            this.radWebDev.TabIndex = 6;
+            this.radWebDev.Text = "Web Dev";
+            this.radWebDev.UseVisualStyleBackColor = true;
+            this.radWebDev.CheckedChanged += new System.EventHandler(this.radWebDev_CheckedChanged);
+            // 
+            // radSystems
+            // 
+            this.radSystems.AutoSize = true;
+            this.radSystems.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radSystems.Location = new System.Drawing.Point(226, 172);
+            this.radSystems.Name = "radSystems";
+            this.radSystems.Size = new System.Drawing.Size(151, 22);
+            this.radSystems.TabIndex = 7;
+            this.radSystems.Text = "Systems Design";
+            this.radSystems.UseVisualStyleBackColor = true;
+            this.radSystems.CheckedChanged += new System.EventHandler(this.radSystems_CheckedChanged);
+            // 
+            // chkBxAdvisor
+            // 
+            this.chkBxAdvisor.AutoSize = true;
+            this.chkBxAdvisor.Location = new System.Drawing.Point(319, 231);
+            this.chkBxAdvisor.Name = "chkBxAdvisor";
+            this.chkBxAdvisor.Size = new System.Drawing.Size(18, 17);
+            this.chkBxAdvisor.TabIndex = 8;
+            this.chkBxAdvisor.TabStop = false;
+            this.chkBxAdvisor.UseVisualStyleBackColor = true;
+            this.chkBxAdvisor.CheckedChanged += new System.EventHandler(this.chkBxAdvisor_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(268, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Spoken To Advisor (Check if Yes):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(370, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "If not, choose date range for possible meeting:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(334, 569);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 30);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 572);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(267, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Number of classes registered for:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(248, 634);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 56);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(67, 634);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 56);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtBxName
+            // 
+            this.txtBxName.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxName.Location = new System.Drawing.Point(134, 59);
+            this.txtBxName.Name = "txtBxName";
+            this.txtBxName.Size = new System.Drawing.Size(285, 30);
+            this.txtBxName.TabIndex = 16;
+            this.txtBxName.TextChanged += new System.EventHandler(this.txtBxName_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(18, 324);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(351, 30);
+            this.dateTimePicker1.TabIndex = 17;
+            this.dateTimePicker1.Value = new System.DateTime(2018, 4, 17, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // SurveyForm
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(441, 728);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtBxName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkBxAdvisor);
+            this.Controls.Add(this.radSystems);
+            this.Controls.Add(this.radWebDev);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radCSharp);
+            this.Controls.Add(this.label1);
+            this.Name = "SurveyForm";
+            this.Text = "Student Questionnaire";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radCSharp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radWebDev;
+        private System.Windows.Forms.RadioButton radSystems;
+        private System.Windows.Forms.CheckBox chkBxAdvisor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtBxName;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ SurveyForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Windows.Forms;
+
+namespace Proj7SurveyForm
+{
+    public partial class SurveyForm : Form
+    {
+        //private static object secondStatement;
+
+        public SurveyForm()
+        {
+            InitializeComponent();
+        }
+
+        //Name Textbox
+        private void txtBxName_TextChanged(object sender, EventArgs e)
+        {
+            this.txtBxName.Text = txtBxName.Text;
+        }
+
+        //C# Radio Button
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Web Dev Radio Button
+        private void radWebDev_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Systems radio button
+        private void radSystems_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Advisor Checkbox
+        private void chkBxAdvisor_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Submit Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you for completing this survey "
+                + txtBxName.ToString() + ". Your appointment with your advisor is set for "
+                + dateTimePicker1.ToString() + ". You are currently registered for "
+                + numericUpDown1.Value.ToString() + " classes this semester. Good luck!");
+            //ReturnSubmitButtonString;
+            if (chkBxAdvisor.Checked)
+                MessageBox.Show("Also, good job speaking with your advisor!");
+            if (radSystems.Checked)
+                MessageBox.Show("You're Going To Love Systems Design!");
+            if (radWebDev.Checked)
+                MessageBox.Show("Have Fun In Web Dev!");
+            if (radCSharp.Checked)
+                MessageBox.Show("Good Luck in C#!");
+        }
+
+        //Calendar
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime dt = dateTimePicker1.Value;
+        }
+
+        //Numeric Up Down Scroll Box
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Value.ToString();
+        }
+
+        //Clear Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtBxName.Text = "";
+            radCSharp.Checked = false;
+            radWebDev.Checked = false;
+            radSystems.Checked = false;
+            numericUpDown1.TextAlign = 0;
+            chkBxAdvisor.Text = "";
+        }
+        #region/* //Delegate to replace Submit Button string   */    
+   //static void Main(string[] args)
+   //     {            
+   //         ReturnSubmitButtonString saying1 = new
+   //             ReturnSubmitButtonString(AHeading);
+   //         ReturnSubmitButtonString saying2 = new
+   //             ReturnSubmitButtonString(txtBxName.ToString());
+   //         ReturnSubmitButtonString saying3 = new
+   //             ReturnSubmitButtonString(secondStatement);
+   //         ReturnSubmitButtonString saying4 = new
+   //             ReturnSubmitButtonString(dateTimePicker1.ToString());
+   //         ReturnSubmitButtonString saying5 = new
+   //             ReturnSubmitButtonString(ThirdStatement);
+   //         ReturnSubmitButtonString saying6 = new
+   //             ReturnSubmitButtonString(numericUpDown1.Value.ToString());
+   //         ReturnSubmitButtonString saying7 = new
+   //             ReturnSubmitButtonString(EndStatement);
+   //         WriteLine(AHeading() + saying2() + saying3() + saying4() + saying5() + saying6() + saying7());
+   //         ReadLine();
+   //     }
+
+   //     private class ReturnSubmitButtonString
+   //     {   //Intellisense generated code
+   //         private Func<string> aHeading;
+   //         private object secondStatement;
+
+   //         public ReturnSubmitButtonString(Func<string> aHeading)
+   //         {
+   //             this.aHeading = aHeading;
+   //         }
+
+   //         public ReturnSubmitButtonString(object secondStatement)
+   //         {
+   //             this.secondStatement = secondStatement;
+   //         }
+   //     }
+    
+
+   //     //Method to return string
+   //     static string AHeading()
+   //     {
+   //         return "Thank you for completing this survey ";
+   //     }
+
+   //     //Method to return 2nd statement in submit button string sequence
+   //     static string SecondStatement()
+   //     {
+   //         return ". Your appointment with your advisor is set for ";
+   //     }
+
+   //     //Method to return third statement in sequence
+   //     static string ThirdStatement()
+   //     {
+   //         return ". You are currently registered for ";
+   //     }
+
+   //     //Method to return end statement in sequence
+   //     static string EndStatement()
+   //     {
+   //         return " classes this semester. Good luck!";
+   //     }
+        #endregion 
+    }//End of Class
+
+}//End of Solution
